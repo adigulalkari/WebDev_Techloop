938bbfb0d9749fafeba7e8f0366aa4f7ee9b2298
adigulalkari <adigulalkari825@gmail.com>
2023-08-10 11:11:34 +0530

    Initial COmmit

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Length,EqualTo,DataRequired
+
+class RegisterForm(FlaskForm):
+    name = StringField(label = 'Name',validators=[Length(min=2,max=30),DataRequired()])
+    password1 = PasswordField(label = 'Password',validators=[Length(min=6, max=30),DataRequired()])
+    password2 = PasswordField(label = 'Cofirm Password',validators=[EqualTo('password1'),DataRequired()])
+    submit = SubmitField(label = 'Submit!')
+
+class LoginForm(FlaskForm):
+    name = StringField(label='Name',validators=[DataRequired()])
+    password = PasswordField(label='Password',validators=[DataRequired()])
+    submit = SubmitField(label='Log In!')
+
+
+
+
---
+++ static/script.js
@@ -0,0 +1,46 @@
+import quizData from "./quizData.js"
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+function loadQuiz() {
+    // lets load the quiz
+}
+
+loadQuiz()
+
+
+// Explain the importance of this function as how the next page retains the selected option
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    
+    // View the results
+    // Add the score to the database
+    // Retake the quiz
+    // View the scoreboard
+
+})
